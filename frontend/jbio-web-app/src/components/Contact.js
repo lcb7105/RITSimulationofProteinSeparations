c2d94fab75ccb832e7323cb8ebc9b52fae57bded
Toula Elwell <tae9467@rit.edu>
2025-02-04 18:20:15 -0500

    finished reformatting the html I have from the previous peoples code for contact page, will add more later

--- frontend/jbio-web-app/src/components/Contact.js
+++ frontend/jbio-web-app/src/components/Contact.js
@@ -2,58 +2,76 @@ function Contact() {
     return (
         <div>
             <h1 className="page-header">Contact</h1>
-            <div className="grid-container">
-                <div class="grid-item">
-                    <h2 className="section-header">Team Members</h2>
+            <section className="coachSponsorContactContainer">
+                <div>
+                    <h2>Team Coach</h2>
+                    <h3>Mark Wilson</h3>
+                    <hr></hr>
+                    <img></img>
                     <ul>
-                        <li>Amr Mualla</li>
-                        <ul>
-                            <li>Email: am3576@rit.edu</li>
-                            <li>Phone: (347) 631-7359</li>
-                        </ul>
-                        <li>Beck Anderson</li>
-                        <ul>
-                            <li>Email: bea1935@rit.edu</li>
-                            <li>Phone: (716) 640-2894</li>
-                        </ul>
-                        <li>Chase Amador</li>
-                        <ul>
-                            <li>Email: cma6320@rit.edu</li>
-                            <li>Phone: (203) 725-4442</li>
-                        </ul>
-                        <li>Landon Heatly</li>
-                        <ul>
-                            <li>Email: lbh1442@rit.edu</li>
-                            <li>Phone: (203) 832-9841</li>
-                        </ul>
-                        <li>Mack Leonard</li>
-                        <ul>
-                            <li>Email: mml2034@rit.edu</li>
-                            <li>Phone: (203) 731-9620</li>
-                        </ul>
+                        <li>RIT Email: <a href="mailto:mwvse@rit.edu" target="_blank" title="Click to Email Mark using his RIT email">mwvse@rit.edu</a></li>
+                        <li>Personal Email: <a href="mailto:mwilson1962@gmail.com" target="_blank" title="Click to Email Mark using his PERSONAL email">mwilson1962@gmail.com</a></li>
                     </ul>
                 </div>
-                <div class="grid-item">
-                    <h2 className="section-header">Team Coach</h2>
+                <div>
+                    <h2>Project Sponsor</h2>
+                    <h3>Dr. Paul Craig</h3>
+                    <img></img>
+                    <hr></hr>
                     <ul>
-                        <li>Mark Wilson</li>
-                        <ul>
-                            <li>RIT Email: mwvse@rit.edu</li>
-                            <li>Personal Email: mwilson1962@gmail.com</li>
-                        </ul>
+                        <li>RIT Email: <a href="mailto:pac8612@rit.edu" target="_blank" title="Click to Email Paul using his RIT email">pac8612@rit.edu</a></li>
+                        <li>Phone: <a href="tel:+15854756145" title="Click to call Paul if you're on your phone">+1(585)475-6145</a></li>
                     </ul>
                 </div>
-                <div class="grid-item">
-                    <h2 className="section-header">Project Sponsor</h2>
+            </section>
+
+            <section className="currentTeamMembers">
+
+            </section>
+
+            <section className="pastTeamMembers">
+                
+                <h2 className="section-header">Past Team Members</h2>
+                <div>
+                    <h3>Amr Mualla</h3>
                     <ul>
-                        <li>Dr. Paul Craig</li>
-                        <ul>
-                            <li>Email: pac8612@rit.edu</li>
-                            <li>Office Phone: (585) 475-6145</li>
-                        </ul>
+                        <li>RIT Email: am3576@rit.edu</li>
+                        <li>Phone: <a href="tel:+13476317359" title="Click to call Amr if you're on your phone">+1(347)631-7359</a></li>
                     </ul>
                 </div>
-            </div>
+
+                <div>
+                    <h3>Beck Anderson</h3>
+                    <ul>
+                        <li>RIT Email: bea1935@rit.edu</li>
+                        <li>Phone: <a href="tel:+17166402894" title="Click to call Beck if you're on your phone">+1(716)640-2894</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Chase Amador</h3>
+                    <ul>
+                        <li>RIT Email: cma6320@rit.edu</li>
+                        <li>Phone: <a href="tel:+12037254442" title="Click to call Chase if you're on your phone">+1(203)725-4442</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Landon Heatly</h3>
+                    <ul>
+                        <li>RIT Email: lbh1442@rit.edu</li>
+                        <li>Phone: <a href="tel:+12038329841" title="Click to call Landon if you're on your phone">+12038329841</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Mack Leonard</h3>
+                    <ul>
+                        <li>RIT Email: mml2034@rit.edu</li>
+                        <li>Phone: <a href="tel:+12037319620" title="Click to call Mack if you're on your phone">+1(203)731-9620</a></li>
+                    </ul>
+                </div>
+            </section>
         </div>
     );
 }
