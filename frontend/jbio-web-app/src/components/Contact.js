5ccc9634deb89e7b3a3e3380b39b57eae0386a94
Toula Elwell <tae9467@rit.edu>
2025-02-05 17:32:06 -0500

    finished reformatting the html I have from the previous peoples code for contact page, will add more later

--- frontend/jbio-web-app/src/components/Contact.js
+++ frontend/jbio-web-app/src/components/Contact.js
@@ -26,7 +26,46 @@ function Contact() {
             </section>
 
             <section className="currentTeamMembers">
+            <h2>Current Team Members</h2>
+                <div>
+                    <h3>Toula Elwell</h3>
+                    <ul>
+                        <li>RIT Email: <a href="mailto:tae9467@rit.edu" target="_blank" title="Click to Email Toula using her RIT email">tae9467@rit.edu</a></li>
+                        <li>Phone: <a href="tel:+15852002069" title="Click to call Toula if you're on your phone">+1(585)200-2069</a></li>
+                    </ul>
+                </div>
 
+                <div>
+                    <h3>Cameron Miele</h3>
+                    <ul>
+                        <li>RIT Email: <a href="mailto:clm1886@rit.edu" target="_blank" title="Click to Email Cameron using his RIT email">clm1886@rit.edu</a></li>
+                        <li>Phone: <a href="tel:+1" title="Click to call Cameron if you're on your phone">+1</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Bryson VerDow</h3>
+                    <ul>
+                        <li>RIT Email: <a href="mailto:bjv7412@rit.edu" target="_blank" title="Click to Email Bryson using his RIT email">bjv7412@rit.edu</a></li>
+                        <li>Phone: <a href="tel:+1" title="Click to call Bryson if you're on your phone">+1</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Castor Grey</h3>
+                    <ul>
+                        <li>RIT Email: <a href="mailto:lcb7105@rit.edu" target="_blank" title="Click to Email Castor using his RIT email">lcb7105@rit.edu</a></li>
+                        <li>Phone: <a href="tel:+1" title="Click to call Castor if you're on your phone">+1</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Jackson Murphy</h3>
+                    <ul>
+                        <li>RIT Email: <a href="mailto:jmm5915@rit.edu" target="_blank" title="Click to Email Jackson using his RIT email">jmm5915@rit.edu</a></li>
+                        <li>Phone: <a href="tel:+1" title="Click to call Jackson if you're on your phone">+1</a></li>
+                    </ul>
+                </div>
             </section>
 
             <section className="pastTeamMembers">
@@ -35,7 +74,7 @@ function Contact() {
                 <div>
                     <h3>Amr Mualla</h3>
                     <ul>
-                        <li>RIT Email: am3576@rit.edu</li>
+                        <li>RIT Email: <a href="mailto:am3576@rit.edu" target="_blank" title="Click to Email Amr using their RIT email">am3576@rit.edu</a></li>
                         <li>Phone: <a href="tel:+13476317359" title="Click to call Amr if you're on your phone">+1(347)631-7359</a></li>
                     </ul>
                 </div>
@@ -43,7 +82,7 @@ function Contact() {
                 <div>
                     <h3>Beck Anderson</h3>
                     <ul>
-                        <li>RIT Email: bea1935@rit.edu</li>
+                        <li>RIT Email: <a href="mailto:bea1935@rit.edu" target="_blank" title="Click to Email Beck using their RIT email">bea1935@rit.edu</a></li>
                         <li>Phone: <a href="tel:+17166402894" title="Click to call Beck if you're on your phone">+1(716)640-2894</a></li>
                     </ul>
                 </div>
@@ -51,7 +90,7 @@ function Contact() {
                 <div>
                     <h3>Chase Amador</h3>
                     <ul>
-                        <li>RIT Email: cma6320@rit.edu</li>
+                        <li>RIT Email: <a href="mailto:cma6320@rit.edu" target="_blank" title="Click to Email Chase using their RIT email">cma6320@rit.edu</a></li>
                         <li>Phone: <a href="tel:+12037254442" title="Click to call Chase if you're on your phone">+1(203)725-4442</a></li>
                     </ul>
                 </div>
@@ -59,15 +98,15 @@ function Contact() {
                 <div>
                     <h3>Landon Heatly</h3>
                     <ul>
-                        <li>RIT Email: lbh1442@rit.edu</li>
-                        <li>Phone: <a href="tel:+12038329841" title="Click to call Landon if you're on your phone">+12038329841</a></li>
+                        <li>RIT Email: <a href="mailto:lbh1442@rit.edu" target="_blank" title="Click to Email Landon using their RIT email">lbh1442@rit.edu</a></li>
+                        <li>Phone: <a href="tel:+12038329841" title="Click to call Landon if you're on your phone">+1(203)832-9841</a></li>
                     </ul>
                 </div>
 
                 <div>
                     <h3>Mack Leonard</h3>
                     <ul>
-                        <li>RIT Email: mml2034@rit.edu</li>
+                        <li>RIT Email: <a href="mailto:mml2034@rit.edu" target="_blank" title="Click to Email Mack using their RIT email">mml2034@rit.edu</a></li>
                         <li>Phone: <a href="tel:+12037319620" title="Click to call Mack if you're on your phone">+1(203)731-9620</a></li>
                     </ul>
                 </div>
