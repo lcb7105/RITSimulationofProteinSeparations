b19bdf22ec3f3f74ff94279e6b0b7cbfbc30d7ab
Castor Grey <lcb7105@rit.edu>
2025-02-11 18:17:53 -0500

    navbar styling, all looks good now

--- frontend/jbio-web-app/src/components/Navbar.js
+++ frontend/jbio-web-app/src/components/Navbar.js
@@ -6,12 +6,13 @@ export default class Navbar extends React.Component{
     return (
         <div className="nav-container">
             <div className={"logo"}>
+                <div className={"temp-icon"}></div>
                 <span id={"nav-logo"}>JBioFramework</span>
             </div>
             <div className={"navbar"}>
                 <ul>
                     <li><NavLink to={"/"} >About</NavLink></li>
-                    <li>Simulators</li>
+                    <li><p>Simulators</p></li>
                     <li><NavLink to={"/instructions"}>Instructions</NavLink></li>
                     <li><NavLink to={"/contact"}>Contact</NavLink></li>
                 </ul>
