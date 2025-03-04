3748d8a6a81ccc44d1a97f823786ac9473fd6f37
Toula Elwell <tae9467@rit.edu>
2025-03-04 17:22:34 -0500

    wrote out base html for the simulators and their code, castor will move the simulations over

--- frontend/jbio-web-app/src/components/SimTwoDE.js
+++ frontend/jbio-web-app/src/components/SimTwoDE.js
@@ -3,7 +3,21 @@ import './SimTwoDE.css';
 function SimTwoDE() {
     return (
         <div>
-            
+            <h1>2D Electrophoresis Simulator</h1>
+            <div className='simulatorBoxTwoDE'>
+                <p>Insert Simulation here</p>
+            </div>
+            <section>
+                <h2>2DE Simulator Instructions</h2>
+                <h3>Steps</h3>
+                <ol>
+                    <li></li>
+                </ol>
+                <h3>Notes</h3>
+                <ul>
+                    <li></li>
+                </ul>
+            </section>
         </div>
     );
 }
